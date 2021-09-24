c4b02d8a8eac21446a4065ca3b8155ae11571028
f.munoz.munoz <f.munoz.munoz@accenture.com>
2021-09-24 14:08:17 +0200

    V2 reset vanilla JS: introduction

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,6 +1,61 @@
 import getApis from "./modules/getAPIs.js";
 import renderSummary from "./modules/renderSummary.js";
 
+const introduction = (data)=>{
+	const scope = document.querySelector(".js-introduction");
+	const youAreNow = scope.querySelector(".js-youAreNow");
+	const youAreHere = scope.querySelector(".js-youAreHere");
+	const youAreUsing = scope.querySelector(".js-youAreUsing");
+	const youBrowser = scope.querySelector(".js-youBrowser");
+	const noSupport = `<span title="Good Luck, your browser respect your privacy" class="strikethrough">notFound</span>`;
+	let urlMap =  window.your.location.latitude ? `https://maps.google.com/maps?q=${window.your.location.latitude},${window.your.location.longitude}` : null;
+	let obj = {
+		previousPage: document.referrer || noSupport,
+		ip: window.your.location.ip || noSupport,
+		country: window.your.location.country_name || noSupport,
+		city: window.your.location.city || noSupport,
+		date: window.your.date.long || noSupport,
+		time: window.your.date.time || noSupport,
+		device: window.your.device.library.model || noSupport,
+		deviceBrand: window.your.device.library.vendor || noSupport,
+		deviceType: window.your.device.library.type || noSupport,
+		ram: window.your.ram || noSupport,
+		cores: window.your.cpu.cores || noSupport,
+		platform: window.your.cpu.platform || noSupport,
+		gpu: window.your.gpu.renderer|| noSupport,
+		os: window.your.os.name + " " + window.your.os.version || noSupport,
+		screen: window.your.screen.px + "px " + window.your.screen.bitsPixel + "bitsPixel"  || noSupport,
+		browserDefault: window.your.browser.default || noSupport,
+		browserName: window.your.browser.library.name + " " + window.your.browser.library.version  || noSupport,
+		darkmode: window.your.darkmode == true ? "enabled" : "disabled" || noSupport,
+		orientation: window.your.orientation || noSupport,
+		plugins: window.your.plugins || noSupport,
+		speed: window.your.speed || noSupport,
+	};
+
+	youAreNow.innerHTML =`
+		Today is <strong title="date">${obj.date}</strong>, and you opened this site at <strong title="time">${obj.time}.</strong>, I know that your ip is <strong title="ip">${obj.ip}</strong>, (the IP is like the phone number of your internet's connection) and you come from the page <strong title="Previous Page">${obj.previousPage}</strong>.
+	`;
+
+	youAreHere.innerHTML =`
+	Now, you are in the country <strong title="country">${obj.country}</strong>, close to the city <strong title="city">${obj.city}</strong>, here in this <a href="${urlMap}">map</a> more or less.
+	`;
+
+	youAreUsing.innerHTML =`You are using a device <strong title="device name">${obj.device}</strong>, with the brand <strong title="brand name">${obj.deviceBrand}</strong>, it is a type of <strong title="type name">${obj.deviceType}</strong>.	I can give you technical information about the device such as the quantity of memory ram <strong title="ram number">${obj.ram}GB</strong>, the CPU <strong title="cpu cores">${obj.cores} cores</strong>  <strong title="cpu platform">${obj.platform}</strong> and complex information like <strong title="gpu name">${obj.gpu}</strong>, the GPU (graphic card) used in your <strong title="device name">${obj.device}</strong>. <br> Don't be scare, but I know that your Operative System is <strong title="OS name and version">${obj.os}</strong> and the resolution of your screen is <strong title="screen resolution and color">${obj.screen}</strong>.
+	`;
+
+	youBrowser.innerHTML =`
+	 Your are using <strong title="browser Default">${obj.browserDefault}</strong>, specifically <strong title="browser name">${obj.browserName}</strong>. I see that you have de <strong title="dark mode">Dark Mode ${obj.darkmode}</strong>. The orientation of the <strong title="device name">${obj.device}</strong> is <strong title="orientation">${obj.orientation}</strong> with this list of plugins installed:  <strong title="plugins names">${obj.plugins}</strong> and a connection speed of <strong title="speed">${obj.speed}</strong>.
+	`;
+	
+	const batteryRenderText = ()=>{
+		
+	};
+
+	batteryRenderText();
+	
+};
+
 /**
  * INIT
  */
@@ -8,7 +63,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 	window.your = {};
 	getApis(() => {
-		renderSummary(window.your);
+		introduction();
+		//renderSummary(window.your);
+
 	});
 });
 
