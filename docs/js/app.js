c53338ed13a4259c4a2311ad7a73bbd4567590cf
f.munoz.munoz <f.munoz.munoz@accenture.com>
2021-10-20 18:06:17 +0200

    V3 header and categries

--- docs/js/app.js
+++ docs/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 	window.your = {};
 	getApis(() => {
-		//renderDomByLang();
+		renderDomByLang();
 	});
 });
 
