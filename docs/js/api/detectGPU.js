f728ef93d3cf5372ebab52a594c7c21ef081ab44
f.munoz.munoz <f.munoz.munoz@accenture.com>
2021-10-20 14:15:18 +0200

    V3 header

--- docs/js/api/detectGPU.js
+++ docs/js/api/detectGPU.js
@@ -13,11 +13,9 @@ const detectGPU = () => {
 
 	const prepareCanvas = (canvas) => {
 		let selector = document.getElementById("ikaycanvas");
+		let selectorCanvas = document.querySelector("#canvasikay") !== null ? document.querySelector("#canvasikay") : document.body;
 		if (selector === null) {
-			document.body.insertAdjacentHTML(
-				"beforeend",
-				`<canvas id="ikaycanvas" width="1" height="1"></canvas>`
-			);
+			selectorCanvas.insertAdjacentHTML("beforeend", `<canvas id="ikaycanvas" width="1" height="1"></canvas>`);
 			let canvas = document.querySelector("canvas#ikaycanvas");
 			gl = canvas.getContext("experimental-webgl");
 			gl.viewportWidth = canvas.width;
@@ -28,12 +26,8 @@ const detectGPU = () => {
 		if (gl) {
 			extension = gl.getExtension("WEBGL_debug_renderer_info");
 			if (extension !== undefined) {
-				gpuObj.vendor = gl.getParameter(
-					extension.UNMASKED_VENDOR_WEBGL
-				);
-				gpuObj.renderer = gl.getParameter(
-					extension.UNMASKED_RENDERER_WEBGL
-				);
+				gpuObj.vendor = gl.getParameter(extension.UNMASKED_VENDOR_WEBGL);
+				gpuObj.renderer = gl.getParameter(extension.UNMASKED_RENDERER_WEBGL);
 			} else {
 				gpuObj.vendor = gl.getParameter(gl.VENDOR);
 				gpuObj.renderer = gl.getParameter(gl.RENDERER);
